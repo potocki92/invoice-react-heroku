b6fa8e862692fd0b54c4ea0ecb4eaaadd8554e75
potocki92 <mateusz.potocki92@gmail.com>
2023-02-26 22:30:25 +0100

    a

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,41 +2,43 @@ const User = require("../models/userModel.js");
 
 // Login User
 const loginUser = (req, res) => {
-const { email, password } = req.body;
-User.findOne({ email: email }, (err, user) => {
-if (user) {
-if (password === user.password) {
-res.send({ message: "Login Successfull", user: user });
-} else {
-res.send({ message: "Password didn't match" });
-}
-} else {
-res.send("User not registered");
-}
-});
+  const { email, password } = req.body;
+  console.log("Firebase Login");
+  User.findOne({ email: email }, (err, user) => {
+    if (user) {
+      if (password === user.password) {
+        res.send({ message: "Login Successfull", user: user });
+      } else {
+        res.send({ message: "Password didn't match" });
+      }
+    } else {
+      res.send("User not registered");
+    }
+  });
 };
 
 // Register User
 const registerUser = (req, res) => {
-const { name, email, password } = req.body;
-User.findOne({ email: email }, (err, user) => {
-if (user) {
-res.send({ message: "User already registered" });
-} else {
-const user = new User({
-name,
-email,
-password,
-});
-user.save((err) => {
-if (err) {
-res.send(err);
-} else {
-res.send({ message: "Successfully Registered" });
-}
-});
-}
-});
+  const { name, email, password } = req.body;
+  console.log("asdad");
+  User.findOne({ email: email }, (err, user) => {
+    if (user) {
+      res.send({ message: "User already registered" });
+    } else {
+      const user = new User({
+        name,
+        email,
+        password,
+      });
+      user.save((err) => {
+        if (err) {
+          res.send(err);
+        } else {
+          res.send({ message: "Successfully Registered" });
+        }
+      });
+    }
+  });
 };
 
 module.exports = { loginUser, registerUser };
--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const routes = require("./routes.js");
 const path = require("path");
+const dotenv = require("dotenv");
 
+dotenv.config();
 const uri = process.env.MONGODB_URI;
 
 const app = express();
@@ -21,6 +23,8 @@ mongoose
 app.use("/", routes);
 app.use(express.static(path.join(__dirname, "build")));
 
-app.listen(PORT, () => {
-  console.log(`Server listening on http://localhost:${PORT}`);
+const port = process.env.PORT || 3001;
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${PORT}`);
 });
