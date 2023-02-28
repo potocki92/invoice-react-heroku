72da17332f9580d281ab58fe3a55a665293bcf09
potocki92 <mateusz.potocki92@gmail.com>
2023-02-28 11:11:15 +0100

    Update userModel

--- models/userModel.js
+++ models/userModel.js
@@ -1,12 +1,10 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-name: { type: String, require: true },
-email: { type: String, require: true },
-password: { type: String, require: true },
-products: { type: Array, default: [] },
-clients: { type: Array, default: [] },
-invoices: { type: Array, default: [] },
+  user: { type: Object, default: {} },
+  products: { type: Array, default: [] },
+  clients: { type: Array, default: [] },
+  invoices: { type: Array, default: [] },
 });
 
 const User = new mongoose.model("User", userSchema);
